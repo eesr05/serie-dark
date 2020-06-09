e4c9b8c02aa85217235443baa138a87813b9e031
eesr05 <evelinne_@hotmail.com>
2020-06-09 16:19:32 -0300

    Adicionando

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// function mostrarIdioma(){
+//     document.getElementById("list-idioma").style.display = "block "      
+
+    
+    
+    
+//     // 
+   
+// }
+
+function mostrarIdioma() {
+    var x = document.getElementById("list-idioma");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
